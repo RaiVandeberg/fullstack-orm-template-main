5ebab6076659e63f9f5fa326a12066b3a7d10b36
Raí <devvandenberg>
2025-02-24 20:54:04 -0300

    estudando prisma - fazendo um CRUD utilizando o prisma

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express"
+import { prisma } from "@/prisma"
+
+class UsersController {
+  async index(request: Request, response: Response) {
+    // findMany retorna todos os registros da tabela
+    const users = await prisma.user.findMany()
+    return response.json(users)
+  }
+
+  async create(request: Request, response: Response, ) {
+
+      const { name, email } = request.body
+
+      await prisma.user.create({data: {name, email}})
+      return response.status(201).json()
+    
+  }
+
+  async show(request: Request, response: Response) {
+    return response.json()
+  }
+}
+
+export { UsersController }
